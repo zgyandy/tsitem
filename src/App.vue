b9343572ae80e72d549791c333f3a84ffd9e5e1a
zgy <guiyu.zhang@haoyishu.org>
2020-07-17 18:19:05 +0800

    添加cypress

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,98 @@
+
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">{{$store.state.state}}</router-link>
+  <div>
+    <!-- <van-address-edit
+    :area-list="areaList"
+    :address-info="addressInfo"
+    show-postal
+    show-delete
+    show-set-default
+    show-search-result
+    :search-result="searchResult"
+    @save="onSave"
+    @delete="onDelete"
+    @change-detail="onChangeDetail"
+    /> -->
+    <div><span>公司名称：</span><input v-model="comName"/></div>
+    <div><span>姓名：</span><input v-model="name"/></div>
+    <div><span>移动电话：</span><input v-model="phone"/></div>
+    <div style="font-size: 14px"><span style="margin-left: 13px">公司地址：</span><input class="addr-input" v-model="province" @click="show" />
+      <input class="addr-input" v-model="city" @click="show"/>
+      <input class="addr-input" v-model="district" @click="show"/>
+      <!-- <van-field
+        v-model="street"
+        type="text"
+        placeholder="街道门牌、楼层房间号等信息"
+      /> -->
     </div>
-    <router-view/>
+    <div v-show="flag">
+      <van-area :area-list="areaList" :item-height=25 @confirm="onAddrConfirm" @cancel="shut" :value="addrCode"/>
+    </div>
   </div>
 </template>
+
 <script>
+import area from '@/assets/js/area.js';
 export default {
   data () {
     return {
-      message: process.env.VUE_APP_SECRET
+      comName: {},
+      searchResult: '',
+      addrCode: '440105',
+      province: '',
+      city: '',
+      district: '',
+      street: '',
+      companyName: '',
+      name: '',
+      phone: '',
+      address: '',
+      areaList: area,
+      flag: false,
+      addressInfo: ''
+      // flag: true
     }
   },
-  mounted () {
-    console.log(process.env.NODE_ENV)
+  created () {
+    this.init()
+  },
+  methods: {
+    init () {
+      // // 初始化地址选择器
+      // this.areaList = areaList.areaList // 初始化选择器数据
+      // 初始化复选框
+      // this.allMajor = this.getMajor()
+    },
+    show () {
+      this.flag = true
+    },
+    onAddrConfirm (e) { // 点击确认，获取所选的省市区数据
+      // 确定选择,返回的必定是三元素数组
+      console.log('this.onAddrConfirm=>e', e)
+      this.province = e[0].name
+      this.city = e[1].name
+      this.district = e[2].name
+      this.flag = false
+    },
+    shut () {
+      this.flag = false
+    },
+    onChangeDetail (val) {
+      if (val) {
+        this.searchResult = [{
+          name: '黄龙万科中心',
+          address: '杭州市西湖区'
+        }]
+      } else {
+        this.searchResult = []
+      }
+    },
+    onSave (e) {
+      console.log(e)
+    },
+    onDelete (e) {
+      console.log(e)
+    }
   }
 }
 </script>
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { Area } from 'vant'
 
+Vue.use(Area)
 Vue.config.productionTip = false
 
 new Vue({
